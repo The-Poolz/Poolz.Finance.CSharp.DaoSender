bac6426de1cf9d4fa42a80cad62ed53c989f161c
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-07-29 19:51:22 +0300

    Add Auth Token (#8)

--- src/Poolz.Finance.CSharp.DaoSender.Generator/Program.cs
+++ src/Poolz.Finance.CSharp.DaoSender.Generator/Program.cs
@@ -3,7 +3,10 @@ using EnvironmentManager.Extensions;
 using Poolz.Finance.CSharp.DaoSender.Generator;
 
 var url = Env.GRAPHQL_URL.GetRequired();
-var schema = await GraphQlGenerator.RetrieveSchema(HttpMethod.Post, url);
+var schema = await GraphQlGenerator.RetrieveSchema(HttpMethod.Post, url, new List<KeyValuePair<string, string>>
+{
+    new("Authorization", Env.GRAPHQL_AUTH_TOKEN.GetRequired())
+});
 var configuration = new GraphQlGeneratorConfiguration
 {
     TargetNamespace = Env.TARGET_NAMESPACE.GetRequired(),
